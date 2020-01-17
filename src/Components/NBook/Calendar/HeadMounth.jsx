6ed9076072f17d7c8b85246ba38cfc9233405158
TEAM\Tyulyukina <Tyulyukina@competentum.ru>
2020-01-17 17:21:07 +0300

    Code rewiew

---
+++ src/Components/NBook/Calendar/HeadMounth.jsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+class HeadMounth extends React.Component {
+    constructor(props){ 
+        super(props);
+
+        this.state = {
+            yBool: props.yearBool, 
+            mBool: props.monthNow,
+            mData: props.mounth
+        };
+    }
+    render(){
+        return (
+            <tr className="name-month">
+                <th className="arrow left" />
+                <th colSpan={(this.state.mBool && 2)||(this.state.yBool && 3)||5}>
+                    { this.state.mData }
+                </th>
+                <th className="arrow right" />
+            </tr>
+        )
+    }
+} 
+
+HeadMounth.propTypes ={
+    yearBool: PropTypes.bool.isRequired,
+    mounthBool: PropTypes.bool.isRequired,
+    mounth: PropTypes.string.isRequired
+}
+
+export default HeadMounth;
