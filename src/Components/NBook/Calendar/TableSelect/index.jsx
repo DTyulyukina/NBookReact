1f8e955eb9d0fe2e14f7770c970958d2a4c53e3e
TEAM\Tyulyukina <Tyulyukina@competentum.ru>
2020-01-29 14:09:09 +0300

    Add life cycle in Calendar

---
+++ src/Components/NBook/Calendar/TableSelect/index.jsx
@@ -0,0 +1,46 @@
+import React from 'react';
+import moment from "moment";
+import PropTypes from 'prop-types';
+
+import HeaderInstruction from './SelectInstruction';
+
+class Headers extends React.Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            weekdaysname: []
+        }
+    }
+    componentWillMount() {
+        const weekdays = moment.weekdays(true);
+        let arrayNameDay = [];
+        weekdays.map((name) => {
+            arrayNameDay.push(<th key={name} className="week-day">{ name }</th>);
+        });
+        this.setState({
+            weekdaysname: arrayNameDay
+        });
+    }
+
+    componentWillUnmount(){
+        this.setState({
+            weekdaysname: []
+        });
+    }
+    
+    render(){
+        return (
+            <tr>
+                { ( this.props.yearBool ? <HeaderInstruction name="year"/> : 
+                  ( this.props.monthBool ? <HeaderInstruction name="mounth"/> : this.state.weekdaysname ))}
+            </tr>
+        );
+    }
+}
+
+Headers.propTypes ={
+    yearBool: PropTypes.bool.isRequired,
+    monthBool: PropTypes.bool.isRequired
+}
+
+export default Headers;
