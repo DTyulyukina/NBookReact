b6382a2aaf117ec001fee70bf7168e2574f6cedb
TEAM\Tyulyukina <Tyulyukina@competentum.ru>
2020-01-30 10:48:48 +0300

    add data in Calendar

--- src/Components/NBook/DailyTimetable/ContainerEvents.jsx
+++ src/Components/NBook/DailyTimetable/ContainerEvents.jsx
@@ -19,32 +19,39 @@ class ContainerEvents extends React.Component {
 
     componentWillMount(){
         const datas = dataNews;
+        let day = this.props.dates.format("L");
+        let daysEvents = [];
+        datas.task.map((line) => {
+            if (line.day === day){
+                daysEvents.push(<NewEvent id={line.id} 
+                                          topEvent={Number(line.top)} 
+                                          leftEvent={Number(line.left)} 
+                                          color={Colors.two}/>);
+            }
+          }
+        );
 
-        let day = this.props.dates.format("YYYY MM DD");
-        if( day === this.props.dates ){
-            this.props.dates.
-            this.setState({
-                componentEvent: <NewEvent  
-                                 topEvent={day.top} 
-                                 leftEvent={day.left} 
-                                 color={Colors.two}
-                                 />
-            });
-        }
-
-        console.log(datas.date);
+        this.setState({
+            componentEvent: daysEvents
+        });
     }
 
-    AddNewRecord(event, dates){ 
+    AddNewRecord(event){ 
         let leftEvent = 5;
         let topEvent = event.pageY;  
         let rect =  event.target.getBoundingClientRect();
+        let containerText = document.getElementsByClassName('texts')[0].children;
+        for( let index=0; index < containerText.length; index++) {
+           let y = topEvent - containerText[index].getBoundingClientRect().top;
+           if( 0 < y && y < 20 ) { 
+            topEvent = containerText[index].getBoundingClientRect().top - rect.top;
+            }
+        }
         this.setState({
             componentEvent: <NewEvent  
                              topEvent={topEvent} 
                              leftEvent={leftEvent} 
-                             color={Colors.two}
-                             rect={rect} />
+                             color={Colors.two}/>
         });
         event.preventDefault();
     }
--- src/Components/NBook/DailyTimetable/NewEvent.jsx
+++ src/Components/NBook/DailyTimetable/NewEvent.jsx
@@ -2,22 +2,13 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 function NewEvent(props){
-    let containerText = document.getElementsByClassName('texts')[0].children;
-    let coordX;
-    for( let index=0; index < containerText.length; index++) {
-        let y = props.topEvent - containerText[index].getBoundingClientRect().top;
-        if( 0 < y && y < 20 ) { 
-           coordX = containerText[index].getBoundingClientRect().top - props.rect.top;
-        }
-    }
     let newStyle = {
-        top: coordX + 'px',
+        top: props.topEvent + 'px',
         left: props.leftEvent + 'px',
         backgroundColor: props.color,
         width: '95%',
         minHeight: '15px'
     };
-    console.log(newStyle);
     return (
         <div className="new-events" style={newStyle}>
             <span className="titel"></span>
@@ -31,8 +22,7 @@ function NewEvent(props){
 NewEvent.propTypes = {
     topEvent: PropTypes.number.isRequired,
     leftEvent: PropTypes.number.isRequired,
-    color: PropTypes.string.isRequired,
-    rect: PropTypes.object.isRequired
+    color: PropTypes.string.isRequired
 }
 
 export default NewEvent;
