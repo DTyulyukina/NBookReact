e942570f9ac426ead9a619ce2206dcce5d70df73
Daria Tiuliukina <daria_tiuliukina@epam.com>
2020-04-24 16:39:49 +0300

    add actions in ListNotes

--- src/Components/NBook/Notes/ListNotes.js
+++ src/Components/NBook/Notes/ListNotes.js
@@ -2,25 +2,48 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 import Notes from './Notes';
+import {addNotes, deleteNotes, updateNotes} from './action';
 
 class ListNotes extends React.Component{
     constructor(props){
         super(props);
 
-        this.state = {
-            arrayNotes: this.props.notes
-        }
+        this.store = this.props.store;
+
+        this.handleAdd = this.handleAdd.bind(this);
+        this.handleDelete = this.handleDelete.bind(this);
+        this.handleUpdate = this.handleUpdate.bind(this);
+    }
+
+    componentDidMount(){
+        this.store.subscribe(() => this.forceUpdate());
+    }
+
+    componentWillMount(){
+        this.store.subscribe(() => this.forceUpdate());
+    }
+
+    handleAdd(text){
+        this.store.dispatch(addNotes(text));
+    }
+
+    handleDelete(id){
+        this.store.dispatch(deleteNotes(id));
+    }
+
+    handleUpdate(id, text){
+        this.store.dispatch(updateNotes(id, text));
     }
 
     render() {
-        let array = this.state.arrayNotes.map((note,index) => {
+        let array = this.store.getState().map((note,index) => {
             return (
                 <React.Fragment key={index}>
                     <Notes key={note.id} id={note.id} text={note.text}/>
                 </React.Fragment>
             )
         });
-        return(array)
+        return(array);
     }
 }
 
--- src/Components/NBook/Notes/index.js
+++ src/Components/NBook/Notes/index.js
@@ -6,21 +6,14 @@ import data from './dates';
 import './Notes.scss';
 
 import reduser from './redusers';
-import {addNotes, deleteNotes, updateNotes} from './action';
 
 const store = createStore(reduser, data);
-
-store.subscribe(() => console.log(store.getState()));
-
-store.dispatch(addNotes('note 6'));
-store.dispatch(deleteNotes(3));
-store.dispatch(updateNotes(2, 'tete'));
 
 class Notes extends React.Component{
     render() {
         return (
             <div className='list_notes'>
-                <ListNotes notes={data} />
+                <ListNotes store={store} />
             </div>
         )
     }
