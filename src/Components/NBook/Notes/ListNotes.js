fa8bd433acd0ff3e5d2a07749510f34cc0b3c6b1
Daria Tiuliukina <daria_tiuliukina@epam.com>
2020-04-27 20:01:06 +0300

    add styles in ListNotes

--- src/Components/NBook/Notes/ListNotes.js
+++ src/Components/NBook/Notes/ListNotes.js
@@ -3,8 +3,16 @@ import React, { Fragment } from 'react';
 import Notes from './Notes';
 
 function ListNotes(props){
+        let num = Math.trunc(props.notes.length/3) + 1;
+        console.log(num)
+        const style = {
+                gridTemplateColumns: 'repeat( 3, 265px)',
+                gridTemplateRows: 'repeat( ' + num + ', 150px)'
+        }
+
+        console.log(style)
         return (
-                <div className='list_notes'>
+                <div className='list_notes' style={style}>
                         {props.notes.map((note, index) =>
                                 <Notes 
                                        key={index} 
